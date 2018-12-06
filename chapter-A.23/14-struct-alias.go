318d515e1359c3ebb18e6a5a5e6a1d84c1702001
novalagung <caknopal@gmail.com>
2018-12-06 09:09:41 +0700

    add type alias

---
+++ chapter-A.23/14-struct-alias.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string `tag1`
+	age  int    `tag2`
+}
+type People = Person
+
+func main() {
+	var p1 = Person{"wick", 21}
+	fmt.Println(p1)
+
+	var p2 = People{"wick", 21}
+	fmt.Println(Person(p2))
+	fmt.Println(People(p1))
+}
