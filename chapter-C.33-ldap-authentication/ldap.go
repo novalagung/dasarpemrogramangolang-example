0835957ca109f7140238f4a8913e6ea6a9e338db
novalagung <caknopal@gmail.com>
2019-06-19 12:15:17 +0700

    add ldap example

---
+++ chapter-C.33-ldap-authentication/ldap.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-ldap/ldap"
+)
+
+// ldap base credentials and config
+const (
+	ldapServer   = "ldap.forumsys.com"
+	ldapPort     = 389
+	ldapBindDN   = "cn=read-only-admin,dc=example,dc=com"
+	ldapPassword = "password"
+	ldapSearchDN = "dc=example,dc=com"
+)
+
+// Base struct of user data
+type UserLDAPData struct {
+	ID       string
+	Email    string
+	Name     string
+	FullName string
+}
+
+// Authenticate user using username and password
+func AuthUsingLDAP(username, password string) (bool, *UserLDAPData, error) {
+
+	// making first contact
+
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ldapServer, ldapPort))
+	if err != nil {
+		return false, nil, err
+	}
+	defer l.Close()
+
+	// bind using basedn
+
+	err = l.Bind(ldapBindDN, ldapPassword)
+	if err != nil {
+		return false, nil, err
+	}
+
+	// construct search query based on uid/username
+
+	searchRequest := ldap.NewSearchRequest(
+		ldapSearchDN,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		fmt.Sprintf("(&(objectClass=organizationalPerson)(uid=%s))", username),
+		[]string{"dn", "cn", "sn", "mail"},
+		nil,
+	)
+
+	// perform the search, validate it's result
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		return false, nil, err
+	}
+
+	if len(sr.Entries) == 0 {
+		return false, nil, fmt.Errorf("User not found")
+	}
+	entry := sr.Entries[0]
+
+	// verify user password by binding to user dn (with user password)
+
+	err = l.Bind(entry.DN, password)
+	if err != nil {
+		return false, nil, err
+	}
+
+	// (optional) store data
+
+	data := new(UserLDAPData)
+	data.ID = username
+
+	for _, attr := range entry.Attributes {
+		switch attr.Name {
+		case "sn":
+			data.Name = attr.Values[0]
+		case "mail":
+			data.Email = attr.Values[0]
+		case "cn":
+			data.FullName = attr.Values[0]
+		}
+	}
+
+	return true, data, nil
+}
