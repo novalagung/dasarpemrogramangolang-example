2803f3b888f04f2fb37e7766e6a7c2c64f158fdc
gedenata <igedetirtanata@gmail.com>
2020-01-15 10:31:41 +0800

    [Typo] Using text print in bahasa Indonesia

--- chapter-A.14-array/2-inisialisasi-array.go
+++ chapter-A.14-array/2-inisialisasi-array.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var fruits = [4]string{"apple", "grape", "banana", "melon"}
 
-	fmt.Println("Jumlah element \t\t", len(fruits))
-	fmt.Println("Isi semua element \t", fruits)
+	fmt.Println("Jumlah elemen \t\t", len(fruits))
+	fmt.Println("Isi semua elemen \t", fruits)
 }
