06d279f2ec9d616c2d612d52070ba135667890ff
novalagung <hello@novalagung.com>
2026-06-11 23:43:50 +0700

    feat: update websocket

--- chapter-D.3-golang-web-socket-chatting-app/go.mod
+++ chapter-D.3-golang-web-socket-chatting-app/go.mod
@@ -1,8 +1,8 @@
 module chapter-D.3
 
-go 1.20
+go 1.26.4
 
 require (
-	github.com/gorilla/websocket v1.4.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/novalagung/gubrak/v2 v2.0.0
 )
--- chapter-D.3-golang-web-socket-chatting-app/main.go
+++ chapter-D.3-golang-web-socket-chatting-app/main.go
@@ -17,6 +17,11 @@ const MESSAGE_NEW_USER = "New User"
 const MESSAGE_CHAT = "Chat"
 const MESSAGE_LEAVE = "Leave"
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 var connections = make([]*WebSocketConnection, 0)
 
 type SocketPayload struct {
@@ -46,7 +51,7 @@ func main() {
 	})
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		currentGorillaConn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+		currentGorillaConn, err := upgrader.Upgrade(w, r, w.Header())
 		if err != nil {
 			http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 			return
@@ -56,14 +61,14 @@ func main() {
 		currentConn := WebSocketConnection{Conn: currentGorillaConn, Username: username}
 		connections = append(connections, &currentConn)
 
-		go handleIO(&currentConn, connections)
+		go handleIO(&currentConn)
 	})
 
 	fmt.Println("Server starting at :8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func handleIO(currentConn *WebSocketConnection, connections []*WebSocketConnection) {
+func handleIO(currentConn *WebSocketConnection) {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("ERROR", fmt.Sprintf("%v", r))
